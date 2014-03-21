2bf3cd7e039fc343e263cbbf72920ae5f2ee4558
Tom Gurion <nagasaki45@gmail.com>
2014-03-21 21:42:34 +0200

    Adding audio.py, Ilan code

---
+++ audio.py
@@ -0,0 +1,39 @@
+import urllib2
+s = 'http://translate.google.com/translate_tts?tl=en&q='
+ 
+def word_to_url(word):
+    return s + urllib2.quote(word.encode('utf8'))
+ 
+def get_mp3_file(address, item):
+    url = address
+    request = urllib2.Request(url)
+    request.add_header('User-agent', 'Mozilla/5.0')
+    opener = urllib2.build_opener()
+    f = open(item+".mp3", "wb")
+    f.write(opener.open(request).read())
+    f.close()
+ 
+def play_sound(name):
+    from subprocess import call
+    # for linux: mpg123
+    # for mac: afplay
+    call(['mpg123', item +'.mp3'])
+ 
+if __name__ == "__main__":
+    for item in ['hello',
+                 'how are you feeling?',
+                 'tired','was it hard?',
+                 'beep',
+                 'we should do it again',
+                 'sure',
+                 'have a great week',
+                 'you too','goodbye',
+                 'thank you',
+                 'where is the bathroom?',
+                 'i need a drink',
+                 'i',
+                 'you',
+                 'pardon my english']:
+        address = word_to_url(item)
+        # get_mp3_file(address,item)
+        play_sound(item)
