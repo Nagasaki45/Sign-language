5b238a2402557cf24428f6530baa3c155cd9d6a1
Tom Gurion <nagasaki45@gmail.com>
2016-02-20 13:25:19 +0200

    Basic aiohttp web server

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 import asyncio
 
+from aiohttp import web
 from pythonosc.dispatcher import Dispatcher
 from pythonosc.osc_server import AsyncIOOSCUDPServer
 
-import osc_utils
 import audio_utils
+
+with open('index.html') as f:
+    index_html = f.read().encode('ascii')
 
 
 def say_handler(*args):
@@ -15,13 +18,32 @@ def say_handler(*args):
     audio_utils.say(text)
 
 
-def main():
-    loop = asyncio.get_event_loop()
+def init_osc(loop):
     dispatcher = Dispatcher()
     dispatcher.map("/*", say_handler)
     osc_server = AsyncIOOSCUDPServer(('127.0.0.1', 12000), dispatcher, loop)
-    osc_server.serve()
-    loop.run_forever()
+    osc_server.serve()  # Calls 'run_until_complete' internally
+
+
+async def init_web(loop):
+    app = web.Application()
+    app.router.add_route('GET', '/', index)
+    app.router.add_static('/static', 'static')
+    await loop.create_server(app.make_handler(), '0.0.0.0', 8080)
+
+
+async def index(request):
+    return web.Response(body=index_html)
+
+
+def main():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(init_web(loop))
+    init_osc(loop)  # Calls 'run_until_complete' internally
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
 
 
 if __name__ == "__main__":
